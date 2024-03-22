40a07e43b292ed4fe9aa533248a763fe20dcba64
Norbiox <norbertchmiel.it@gmail.com>
2024-03-22 10:33:44 +0100

    make hymn page opened by clicking on hymn title in hymns list page

--- lib/hymns/hymns_list_page.dart
+++ lib/hymns/hymns_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:spiewnik_pielgrzyma/hymns/hymn_page.dart';
 import 'package:spiewnik_pielgrzyma/hymns/hymns_list.dart';
 
 class HymnsListPage extends StatefulWidget {
@@ -34,8 +35,12 @@ class _HymnsListPageState extends State<HymnsListPage>
             itemCount: hymnsList.length,
             itemBuilder: (context, index) {
               return ListTile(
-                title: Text(hymnsList[index].fullTitle),
-              );
+                  title: Text(hymnsList[index].fullTitle),
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => HymnPage(hymn: hymnsList[index]),
+                    ));
+                  });
             }),
       );
     });
