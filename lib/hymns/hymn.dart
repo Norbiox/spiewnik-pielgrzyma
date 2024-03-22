adf25f533bb6a76251b4378a8f15195e3ce3e7c5
Norbiox <norbertchmiel.it@gmail.com>
2024-03-22 10:32:48 +0100

    load hymn text

--- lib/hymns/hymn.dart
+++ lib/hymns/hymn.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/services.dart';
 
 class Hymn extends Equatable {
   final String number;
@@ -6,11 +7,15 @@ class Hymn extends Equatable {
   final String title;
   final String group;
   final String subgroup;
-  String? text;
 
   Hymn(this.number, this.filename, this.title, this.group, this.subgroup);
 
   String get fullTitle => "$number. $title";
+
+  Future<List<String>> getText() async {
+    final String text = await rootBundle.loadString("assets/texts/" + filename);
+    return text.split('\n').sublist(1);
+  }
 
   @override
   List<Object> get props => [number, filename, title, group, subgroup];
