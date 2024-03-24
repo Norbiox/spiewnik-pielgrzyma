35eadb35a7bc930159e0e71115f72930c231758a
Norbiox <norbertchmiel.it@gmail.com>
2024-03-24 17:46:00 +0100

    add favorite icon to hymn tile

--- lib/hymns/hymn_tile_widget.dart
+++ lib/hymns/hymn_tile_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:spiewnik_pielgrzyma/favorites/repository/shared_preferences.dart';
 import 'package:spiewnik_pielgrzyma/hymns/hymn.dart';
 import 'package:spiewnik_pielgrzyma/hymns/hymn_page.dart';
 
@@ -7,14 +9,33 @@ class HymnTileWidget extends StatelessWidget {
 
   const HymnTileWidget({super.key, required this.hymn});
 
+  Future<IconData> getFavoriteIcon() async {
+    if (await SharedPreferencesFavoritesRepository(
+            await SharedPreferences.getInstance())
+        .isFavorite(hymn.number)) {
+      return Icons.favorite;
+    } else {
+      return Icons.favorite_border;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        title: Text(hymn.fullTitle),
-        onTap: () {
-          Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => HymnPage(hymn: hymn),
-          ));
+    return FutureBuilder(
+        future: getFavoriteIcon(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListTile(
+                leading: Icon(snapshot.data),
+                title: Text(hymn.fullTitle),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => HymnPage(hymn: hymn),
+                  ));
+                });
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
         });
   }
 }
