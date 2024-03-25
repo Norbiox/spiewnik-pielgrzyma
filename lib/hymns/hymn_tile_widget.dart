a6477b6616e39ca2ea71bc62efa603713a2ae3f6
Norbiox <norbertchmiel.it@gmail.com>
2024-03-25 08:21:38 +0100

    use FavoriteIconWidget in hymns list

--- lib/hymns/hymn_tile_widget.dart
+++ lib/hymns/hymn_tile_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:spiewnik_pielgrzyma/favorites/repository/shared_preferences.dart';
+import 'package:spiewnik_pielgrzyma/favorites/icon_widget.dart';
 import 'package:spiewnik_pielgrzyma/hymns/hymn.dart';
 import 'package:spiewnik_pielgrzyma/hymns/hymn_page.dart';
 
@@ -9,33 +8,15 @@ class HymnTileWidget extends StatelessWidget {
 
   const HymnTileWidget({super.key, required this.hymn});
 
-  Future<IconData> getFavoriteIcon() async {
-    if (await SharedPreferencesFavoritesRepository(
-            await SharedPreferences.getInstance())
-        .isFavorite(hymn.number)) {
-      return Icons.favorite;
-    } else {
-      return Icons.favorite_border;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: getFavoriteIcon(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListTile(
-                leading: Icon(snapshot.data),
-                title: Text(hymn.fullTitle),
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => HymnPage(hymn: hymn),
-                  ));
-                });
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
+    return ListTile(
+        leading: FavoriteIconWidget(hymn: hymn),
+        title: Text(hymn.fullTitle),
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => HymnPage(hymn: hymn),
+          ));
         });
   }
 }
