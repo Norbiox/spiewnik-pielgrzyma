4215ccd20c731c4ee502a135ccceb32d12a17496
Norbiox <norbertchmiel.it@gmail.com>
2024-03-28 16:09:46 +0100

    fix tests

--- test/hymns/hymn_test.dart
+++ test/hymns/hymn_test.dart
@@ -4,12 +4,14 @@ import 'package:spiewnik_pielgrzyma/hymns/hymn.dart';
 
 void main() {
   test("fullTitle should contain number and title - arabic number", () {
-    final Hymn hymn = const Hymn("1", "001.txt", "title", "group", "subgroup");
+    final Hymn hymn =
+        const Hymn(0, "1", "001.txt", "title", "group", "subgroup", []);
     expect(hymn.fullTitle, "1. title");
   });
 
   test("fullTitle should contain number and title - roman number", () {
-    final Hymn hymn = const Hymn("X", "00X.txt", "title", "group", "subgroup");
+    final Hymn hymn =
+        const Hymn(0, "X", "00X.txt", "title", "group", "subgroup", []);
     expect(hymn.fullTitle, "X. title");
   });
 }
--- test/hymns/hymns_list_loader_test.dart
+++ test/hymns/hymns_list_loader_test.dart
@@ -1,19 +1,25 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:spiewnik_pielgrzyma/hymns/hymn.dart';
 
-import 'package:spiewnik_pielgrzyma/hymns/hymns_list_loader.dart';
+import 'package:spiewnik_pielgrzyma/hymns/hymns_list.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized(); // needed for access to assets
 
   test('Loads hymns', () {
-    final loader = HymnsListLoader();
-    return loader.loadHymnsList().then((hymns) {
+    return loadHymnsList().then((hymns) {
       expect(hymns, isNotEmpty);
       expect(
         hymns[0],
-        const Hymn("1", "001.txt", "Alleluja, chwalcie Pana", "I. Bóg Trójjedyny",
-            "1. Chwała i dziękczynienie"),
+        const Hymn(
+          0,
+          "1",
+          "001.txt",
+          "Alleluja, chwalcie Pana",
+          "I. Bóg Trójjedyny",
+          "1. Chwała i dziękczynienie",
+          [],
+        ),
       );
     });
   });
