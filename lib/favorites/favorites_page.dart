e0edf2a14d9b454391ae986b7b30a345c80bebc8
Norbiox <norbertchmiel.it@gmail.com>
2024-03-30 11:28:43 +0100

    use HymnsListWidget in favorites hymns page

--- lib/favorites/favorites_page.dart
+++ lib/favorites/favorites_page.dart
@@ -1,50 +1,25 @@
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
-import 'package:spiewnik_pielgrzyma/favorites/repository/abstract.dart';
 import 'package:spiewnik_pielgrzyma/favorites/repository/shared_preferences.dart';
-import 'package:spiewnik_pielgrzyma/hymns/hymn_tile_widget.dart';
+import 'package:spiewnik_pielgrzyma/hymns/hymns_list_widget.dart';
 
-class FavoritesPage extends StatefulWidget {
+class FavoritesPage extends StatelessWidget {
   const FavoritesPage({super.key});
 
   @override
-  State<FavoritesPage> createState() => _FavoritesPageState();
-}
-
-class _FavoritesPageState extends State<FavoritesPage>
-    with AutomaticKeepAliveClientMixin<FavoritesPage> {
-  late FavoritesRepository repository;
-
-  @override
-  void initState() {
-    super.initState();
-    repository = Provider.of<SharedPreferencesFavoritesRepository>(context,
-        listen: false);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    super.build(context);
-
     return Consumer<SharedPreferencesFavoritesRepository>(
         builder: (builder, value, child) {
       return FutureBuilder(
-        future: value.getFavorites(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-                child: Text("Nie masz jeszcze ulubionych pieśni"));
-          }
+          future: value.getFavorites(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                  child: Text("Nie masz jeszcze ulubionych pieśni"));
+            }
 
-          return ListView(
-              children: snapshot.data!
-                  .map((hymn) => HymnTileWidget(hymn: hymn))
-                  .toList());
-        },
-      );
+            return HymnsListWidget(hymnsList: snapshot.data!);
+          });
     });
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
