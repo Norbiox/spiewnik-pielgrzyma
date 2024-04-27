93f8f33a3b1f402aa72f0e26269aa3251d84c762
Norbiox <norbertchmiel.it@gmail.com>
2024-04-27 19:24:02 +0200

    change CustomLists.id field type

--- lib/custom_lists/database/migrations.dart
+++ lib/custom_lists/database/migrations.dart
@@ -2,7 +2,7 @@ import 'package:sqflite/sqflite.dart';
 
 void createCustomListsTable(Batch batch) {
   batch.execute('''CREATE TABLE CustomLists (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id TEXT PRIMARY KEY UNIQUE,
     name TEXT NOT NULL UNIQUE,
     orderingIndex INTEGER NOT NULL UNIQUE,
     createdAt TEXT NOT NULL,
