0e99e9da55a3e15dca542210d29b5fa14a4b5338
Norbiox <norbertchmiel.it@gmail.com>
2024-04-27 20:05:21 +0200

    add factory od CustomList

--- lib/custom_lists/model/list.dart
+++ lib/custom_lists/model/list.dart
@@ -1,9 +1,14 @@
 import 'dart:collection';
+import 'dart:developer';
 
+import 'package:flutter/foundation.dart';
+import 'package:spiewnik_pielgrzyma/custom_lists/database/query.dart';
 import 'package:spiewnik_pielgrzyma/hymns/model/hymn.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:uuid/uuid.dart';
 
 class CustomList {
-  late int id;
+  final String id;
   String name;
   late int orderingIndex;
   late DateTime createdAt;
@@ -14,6 +19,13 @@ class CustomList {
       this.id, this.name, this.orderingIndex, this.createdAt, this.modifiedAt,
       [hymns = const <Hymn>[]]) {
     _hymns = List<Hymn>.from(hymns);
+  }
+
+  factory CustomList.create(String name, int orderingIndex) {
+    Uuid uuid = const Uuid();
+    DateTime now = DateTime.now();
+    return CustomList(
+        uuid.v1(), name, orderingIndex, now, now, List<Hymn>.from([]));
   }
 
   UnmodifiableListView<Hymn> get hymns => UnmodifiableListView(_hymns);
