cfa9ea54141d6d192a07a004b33b78b88219827b
Norbiox <norbertchmiel.it@gmail.com>
2024-10-25 20:12:28 +0200

    Save downloaded hymn PDFs for later use

--- lib/app/providers/hymn_pdf.dart
+++ lib/app/providers/hymn_pdf.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:csv/csv.dart';
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:spiewnik_pielgrzyma/app/providers/hymn_pdf_storage.dart';
 import 'package:spiewnik_pielgrzyma/models/hymn.dart';
 import 'package:spiewnik_pielgrzyma/utils/encryption_service.dart';
 
@@ -11,20 +14,6 @@ abstract class HymnPdfProvider {
   String get loadingMessage;
   Future<Uint8List> getHymnPdfFile(Hymn hymn);
 }
-
-// class AssetsHymnPdfProvider implements HymnPdfProvider {
-//   @override
-//   String get loadingMessage => "Trwa wczytywanie nut...";
-
-//   @override
-//   Future<Uint8List> getHymnPdfFile(Hymn hymn) async {
-//     final String hymnPdfPath =
-//         "assets/pdf/nuty-${hymn.number.toUpperCase()}.pdf";
-//     return await rootBundle
-//         .load(hymnPdfPath)
-//         .then((value) => value.buffer.asUint8List());
-//   }
-// }
 
 class NetworkHymnPdfProvider implements HymnPdfProvider {
   final String linkBase = "https://docs.google.com/uc?export=download&id=";
@@ -39,8 +28,7 @@ class NetworkHymnPdfProvider implements HymnPdfProvider {
     return linkBase + hymnNumberToId[hymn.number.toUpperCase()]!;
   }
 
-  @override
-  Future<Uint8List> getHymnPdfFile(Hymn hymn) async {
+  Future<Uint8List> fetchPdfFile(Hymn hymn) async {
     try {
       final response = await http.get(Uri.parse(getHymnPdfUrl(hymn)));
 
@@ -54,6 +42,11 @@ class NetworkHymnPdfProvider implements HymnPdfProvider {
           "Nie udało się pobrać nut. Upewnij się, że masz połączenie z internetem");
     }
   }
+
+  @override
+  Future<Uint8List> getHymnPdfFile(Hymn hymn) async {
+    return await fetchPdfFile(hymn);
+  }
 }
 
 class DecryptingNetworkHymnPdfProvider extends NetworkHymnPdfProvider {
@@ -63,14 +56,33 @@ class DecryptingNetworkHymnPdfProvider extends NetworkHymnPdfProvider {
       this.encryptionKey, Map<String, String> hymnNumberToId)
       : super(hymnNumberToId);
 
-  @override
-  Future<Uint8List> getHymnPdfFile(Hymn hymn) async {
-    final pdfBytes = await super.getHymnPdfFile(hymn);
+  Future<Uint8List> decryptPdfFile(Uint8List pdf) async {
     final encryptionService = EncryptionService();
     encryptionService.init(encryptionKey);
-    return Uint8List.fromList(encryptionService
-        .decryptData(String.fromCharCodes(pdfBytes))
-        .codeUnits);
+    return Uint8List.fromList(
+        encryptionService.decryptData(String.fromCharCodes(pdf)).codeUnits);
+  }
+
+  @override
+  Future<Uint8List> getHymnPdfFile(Hymn hymn) async {
+    return await fetchPdfFile(hymn).then(decryptPdfFile);
+  }
+}
+
+class DecryptingNetworkHymnPdfProviderWithStorage
+    extends DecryptingNetworkHymnPdfProvider {
+  final HymnPdfStorage hymnPdfStorage;
+
+  DecryptingNetworkHymnPdfProviderWithStorage(this.hymnPdfStorage,
+      String encryptionKey, Map<String, String> hymnNumberToId)
+      : super(encryptionKey, hymnNumberToId);
+
+  @override
+  Future<Uint8List> getHymnPdfFile(Hymn hymn) async {
+    final pdfBytes = await hymnPdfStorage.getHymnPdfFile(hymn.number) ??
+        await fetchPdfFile(hymn);
+    await hymnPdfStorage.saveHymnPdfFile(hymn.number, pdfBytes);
+    return decryptPdfFile(pdfBytes);
   }
 }
 
@@ -86,5 +98,12 @@ Future<HymnPdfProvider> hymnPdfProviderFactory() async {
           .sublist(1)
           .asMap()
           .map((index, entry) => MapEntry(entry[0], entry[1]));
-  return DecryptingNetworkHymnPdfProvider(encryptionKey, hymnNumberToId);
+
+  if (kIsWeb) {
+    return DecryptingNetworkHymnPdfProvider(encryptionKey, hymnNumberToId);
+  } else {
+    final documentsPath = (await getApplicationDocumentsDirectory()).path;
+    return DecryptingNetworkHymnPdfProviderWithStorage(
+        DocumentsHymnPdfStorage(documentsPath), encryptionKey, hymnNumberToId);
+  }
 }
